84b804e60eaf1c33e3efce21e021a3d763107fd4
Matthew Johnston <62052411+MatthewJohnston0905@users.noreply.github.com>
2025-08-17 18:41:54 +0100

    Create disable-autologin.sh

---
+++ scripts/disable-autologin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script is executed during the image build process.
+
+set -oue pipefail
+
+echo "Removing steamos autologin config"
+rm /etc/sddm.conf.d/steamos.conf
+
+echo "Disabling bazzite-autologin service"
+systemctl disable bazzite-autologin.service
+
+# Optional: To remove the on-screen keyboard from the login screen, remove the '#' from the line below
+# rm /etc/sddm.conf.d/virtualkbd.conf
+
+# Optional: To enable desktop-mode auto-updates, remove the '#' from the line below
+# systemctl enable ublue-update.timer
